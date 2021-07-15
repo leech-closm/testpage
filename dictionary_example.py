b2bbf422f90b8f8ce47b42c4397ade76e9814ab2
이찬희 <leech5124@gmail.com>
2021-07-15 14:09:38 +0900

    first commit

---
+++ dictionary_example.py
@@ -0,0 +1,38 @@
+"""
+딕셔너리 실습2
+"""
+
+coin = {
+    'BTC': {
+        'opening_price': '44405000',
+        'closing_price': '38806000',
+        'min_price': '36640000',
+        'max_price': '44999000',
+        'prev_closing_price': '44404000',
+        'fluctate_24H': '-7463000',
+        'fluctate_rate_24H': '-16.13',
+    },
+    'ETH': {
+        'opening_price': '1458000',
+        'closing_price': '1229000',
+        'min_price': '1100000',
+        'max_price': '1490000',
+        'prev_closing_price': '1458000',
+        'fluctate_24H': '-275000',
+        'fluctate_rate_24H': '-18.28',
+    },
+    'XRP': {
+        'opening_price': '364.5',
+        'closing_price': '311.9',
+        'min_price': '284.2',
+        'max_price': '372.7',
+        'prev_closing_price': '364.2',
+        'fluctate_24H': '-90.6',
+        'fluctate_rate_24H': '-22.51',
+    },
+}
+bitcoin = coin['BTC']
+bit_open = int(bitcoin['opening_price'])
+xrp = coin['XRP']
+xrp_open = float(xrp['opening_price'])
+print(bit_open+xrp_open)
